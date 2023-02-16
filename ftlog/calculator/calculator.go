33ac71f83345826c3e01beea5e6c01f89ab68118
Josh Campbell <josh@p4a.net>
2023-02-16 10:31:24 -0800

    weeee

--- ftlog/calculator/calculator.go
+++ ftlog/calculator/calculator.go
@@ -6,3 +6,9 @@ package calculator
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+// Subtract takes two numbers a and b, and
+// returns the result of subtracting b from a.
+func Subtract(a, b float64) float64 {
+	return a - b
+}
